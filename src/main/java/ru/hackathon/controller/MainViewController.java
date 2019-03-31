63d78fd40f407c30c458321389e42860a12246a2
uzaretskaya <JLure@yandex.ru>
2019-03-31 11:50:54 +0300

    Есть таблица на форме, выводит данные

--- src/main/java/ru/hackathon/controller/MainViewController.java
+++ src/main/java/ru/hackathon/controller/MainViewController.java
@@ -4,38 +4,37 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.web.WebView;
 import javafx.util.Duration;
-import org.w3c.dom.NodeList;
 import ru.hackathon.App;
 import ru.hackathon.dao.FromMySql;
-import ru.hackathon.dao.SQLHandler;
 import ru.hackathon.model.Camera;
-
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Stream;
-
-import static javafx.scene.media.MediaPlayer.Status.STOPPED;
-
 
 public class MainViewController implements Initializable {
 
     @FXML
     private MediaView video;
-    @FXML
-    private WebView data;
-
     private Media media;
     private MediaPlayer mediaPlayer;
-    private double currentTime;
 
     private ObservableList<Camera> camerasData = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Camera> tableCameras;
+
+    @FXML
+    private TableColumn<Camera, Long> cameraIDColumn;
+
+    @FXML
+    private TableColumn<Camera, String> videoPathColumn;
 
     private App app;
 
@@ -92,9 +91,9 @@ public class MainViewController implements Initializable {
         }
         FromMySql sqlBase = new FromMySql();
         List<Camera> cameras = sqlBase.getCameras();
-        for (Camera camera : cameras) {
-            System.out.println(camera.getCameraId() + " - " + camera.getPathVideo());
-        }
         camerasData.addAll(cameras);
+        cameraIDColumn.setCellValueFactory(new PropertyValueFactory<Camera, Long>("cameraId"));
+        videoPathColumn.setCellValueFactory(new PropertyValueFactory<Camera, String>("pathVideo"));
+        tableCameras.setItems(camerasData);
     }
 }
